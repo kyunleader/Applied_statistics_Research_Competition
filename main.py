10e79daaf56d4a80edb561ed2adc178fd46cbded
kyun <rlgus7354@gmail.com>
2021-04-29 15:39:45 +0900

    0429

---
+++ main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import font_manager
+import matplotlib
+font_name = font_manager.FontProperties(fname='C:/Windows/Fonts/H2HDRM.TTF').get_name()
+plt.rc('font', family=font_name)
+matplotlib.rcParams['axes.unicode_minus'] = False  # matplotlib 에 한국어 폰트 적용
+
+# 데이터 불러오기
+data = pd.read_excel('data.xlsx')
+
+# 데이터 탐색
+''' 간단하게 해보기 (jupyter 사용)
+import sweetviz as sv
+advert_report = sv.analyze(data)
+sv.analyze(data).show_html('./sweetviz_Advertising.html')'''
+
+# 남여 비교
+sns.set_style('whitegrid')
+colors = ['#FF1E00', '#4380D3']  # 색 설정
+sns.set_palette(colors)
+plt.rc('font', family=font_name)  # 폰트 설정
+sns.countplot(x=['남' if i == 1 else '여' for i in data['sex']])
+
+# 학년 비교
+sns.countplot(x='grade', data=data, palette='Accent')
+plt.xlabel('학년')
+
+# 연애 유무
+sns.countplot(x=['커플' if i == 1 else '솔로' for i in data['couple']], palette='Accent')
+
+# 한달 지출비 분포
+plt.figure(figsize=(9,9))
+for i in range(1,10):
+    print(i)
+    plt.subplot(3, 3, i)
+    sns.distplot(data.iloc[:, i+6])
+
+
+
+
